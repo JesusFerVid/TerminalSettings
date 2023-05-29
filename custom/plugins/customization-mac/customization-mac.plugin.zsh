a2ea84c7fcd13c30c70f0aa2756f3fa64696296a
JesusFerVid <jesusfervid94@gmail.com>
2023-05-29 09:23:27 +0200

    chore(customization-mac): Added useful alias for quick development

--- custom/plugins/customization-mac/customization-mac.plugin.zsh
+++ custom/plugins/customization-mac/customization-mac.plugin.zsh
@@ -5,5 +5,8 @@ alias pat="cat $REPOS_DIR/pat"
 alias pat2="cat $REPOS_DIR/pat2"
 alias tel="cd $REPOS_DIR/Telemetry"
 
+alias health="curl http://0.0.0.0:8080/health" 
+
 # GitHub
 alias prt="code $REPOS_DIR/pr_template.md"
+alias mct="cat $REPOS_DIR/pr_merge_commit_template.txt"
